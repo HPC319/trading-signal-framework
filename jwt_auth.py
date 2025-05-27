f92f7be5c2a408935d34515eae6b8f085063f200
HPC319 <154732424+HPC319@users.noreply.github.com>
2025-05-27 08:55:08 -0500

    Update jwt_auth.py

--- jwt_auth.py
+++ jwt_auth.py
@@ -1,18 +1,1 @@
-import os
-import time
-import jwt  # PyJWT
 
-def create_coinbase_jwt():
-    api_key_id = os.environ["COINBASE_API_KEY_ID"]
-    private_key = os.environ["COINBASE_API_PRIVATE_KEY"]
-
-    payload = {
-        "iss": api_key_id,
-        "sub": api_key_id,
-        "aud": "coinbase-cloud",
-        "iat": int(time.time()),
-        "exp": int(time.time()) + 300
-    }
-
-    token = jwt.encode(payload, private_key, algorithm="ES256")
-    return token
